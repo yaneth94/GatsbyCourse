8392a49ae798080252fc131b7b65ca7da8bf806d
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-03 17:38:37 -0600

    added page in tutorial

---
+++ tutorial/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return <div> 
+  <h1>this is our home pages</h1>
+  Hello world! 
+  <div>
+  </div>
+  </div>
+}
---
+++ tutorial/src/pages/products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Products() {
+    return (
+        <div>
+            <h1>This is products page</h1>
+        </div>
+    )
+}
