953ac8233c2495166aacfbd3b93975097f7448d3
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-12 11:08:02 -0600

    added SimpleHero

--- backroads/src/components/Footer.jsx
+++ backroads/src/components/Footer.jsx
@@ -3,14 +3,14 @@ import styles from "../css/footer.module.css"
 import links from "../constants/links"
 import socialIcons from "../constants/social-icons"
 import { Link } from "gatsby"
-/*import { FaRProject } from "react-icons/fa"*/
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.links}>
         {links.map((item, index) => {
           return (
-            <Link key={index} to={item.mpat}>
+            <Link key={index} to={item.path}>
               {item.text}
             </Link>
           )
--- backroads/src/pages/index.jsx
+++ backroads/src/pages/index.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import SimpleHero from "../components/SimpleHero"
 
 export default function Home() {
   return (
     <Layout>
-      <div>Hello from backroads</div>
+      <SimpleHero></SimpleHero>
     </Layout>
   )
 }
