1f4d2cc3f6101b92999d79ed24dc2f431d3c839d
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-12 11:45:42 -0600

    added banner in index and styles for it

--- backroads/src/pages/index.jsx
+++ backroads/src/pages/index.jsx
@@ -1,11 +1,22 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
 
 export default function Home() {
   return (
     <Layout>
-      <SimpleHero></SimpleHero>
+      <SimpleHero>
+        <Banner
+          title="continue exploring"
+          info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, odio."
+        >
+          <Link to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </SimpleHero>
     </Layout>
   )
 }
