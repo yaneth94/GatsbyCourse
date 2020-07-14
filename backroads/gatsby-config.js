73e7ea759ae6664e9bf05a745eb9d50e9ab05bcd
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-14 11:27:45 -0600

    added images and added config of plugin gastby image and filesystem

--- backroads/gatsby-config.js
+++ backroads/gatsby-config.js
@@ -15,5 +15,16 @@ module.exports = {
             age: 24,
         },
     },
-    plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+    plugins: [{
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images/`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-styled-components`,
+    ],
 }
