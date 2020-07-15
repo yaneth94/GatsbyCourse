0616369966a92b1f9190856a1e9a840d139d59f7
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-15 11:19:22 -0600

    added background-image in StyledHero and Index of backroads

--- backroads/src/pages/blog.jsx
+++ backroads/src/pages/blog.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+/*import Images from "../examples/Images"*/
+
 const blog = () => {
   return (
     <Layout>
       <Link to="/">back home</Link>Hello from blog page
       {/*<h1> title: {data.site.siteMetadata.title}</h1>*/}
+      {/*<Images></Images>*/}
     </Layout>
   )
 }
--- backroads/src/pages/index.jsx
+++ backroads/src/pages/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Layout from '../components/Layout'
-import SimpleHero from '../components/SimpleHero'
-import Banner from '../components/Banner'
-import { Link } from 'gatsby'
-import About from '../components/Home/About'
-import Services from '../components/Home/Services'
+import React from "react"
+import Layout from "../components/Layout"
+//import SimpleHero from "../components/SimpleHero"
+import Banner from "../components/Banner"
+import { Link } from "gatsby"
+import About from "../components/Home/About"
+import Services from "../components/Home/Services"
+import StyledHero from "../components/StyledHero"
+import { graphql } from "gatsby"
 
-export default function Home () {
+export default function Home({ data }) {
   return (
     <Layout>
+      {/*
       <SimpleHero>
         <Banner
           title='continue exploring'
@@ -19,8 +22,30 @@ export default function Home () {
           </Link>
         </Banner>
       </SimpleHero>
+      */}
+      <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
+        <Banner
+          title="continue exploring"
+          info=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, officiis."
+        >
+          <Link fade to="/tours" className="btn-white">
+            explore tours
+          </Link>
+        </Banner>
+      </StyledHero>
       <About></About>
       <Services></Services>
     </Layout>
   )
 }
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
