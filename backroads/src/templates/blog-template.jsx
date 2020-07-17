8a36f887a7cc54d6ba5181c870da2c457c25d18e
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-16 22:33:03 -0600

    added page dinamic for blog and blog-template

---
+++ backroads/src/templates/blog-template.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import styles from '../css/single-blog.module.css'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+const Blog = ({ data }) => {
+  const {
+    title,
+    published,
+    text: { json }
+  } = data.post
+
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <div className={styles.center}>
+          <h1>{title}</h1>
+          <h4>published at : {published}</h4>
+          <article className={styles.post}>
+            {documentToReactComponents(json)}
+          </article>
+          <AniLink fade to='/blog' className='btn-primary'>
+            all posts
+          </AniLink>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query getPost($slug: String!) {
+    post: contentfulPost(slug: { eq: $slug }) {
+      title
+      published(formatString: "MMMM Do, YYYY")
+      text {
+        json
+      }
+    }
+  }
+`
+
+export default Blog
