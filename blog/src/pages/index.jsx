290716a3eeea56a4cebcca1006320664434368b4
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-17 22:19:04 -0600

    added postCard and postList

--- blog/src/pages/index.jsx
+++ blog/src/pages/index.jsx
@@ -1,12 +1,43 @@
-import React from "react"
-import Layout from "../components/Layout"
-import { Link } from "gatsby"
+import React from 'react'
+import Layout from '../components/layout'
+/*import { Link } from 'gatsby'*/
+import PostList from '../components/PostList'
+import { graphql, useStaticQuery } from 'gatsby'
 
-export default function Home() {
+const getPosts = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+            date(formatString: "MMMM Do, YYYY")
+            author
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+export default () => {
+  const response = useStaticQuery(getPosts)
+  const posts = response.allMdx.edges
+  console.log(posts)
+
   return (
     <Layout>
-      <Link to="/post">Post Page</Link>
-      Hola
+      <PostList posts={posts} />
     </Layout>
   )
 }
