be9cd3fb2f1db47586c7571e9363cfe005b1943c
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-19 13:39:55 -0600

    added about page and footer

--- coffee/src/components/layout.jsx
+++ coffee/src/components/layout.jsx
@@ -4,10 +4,11 @@ import PropTypes from "prop-types"
 import "./bootstrap.min.css"
 import "./layout.css"
 import Navbar from "./Globals/Navbar"
+import Footer from "./Globals/Footer"
 
 const Layout = ({ children }) => (
   <>
-    <Navbar></Navbar> {children}{" "}
+    <Navbar></Navbar> {children} <Footer></Footer>
   </>
 )
 
