dbc25efc3632720de53efadc7a56445385baac1e
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-19 10:56:46 -0600

    added navbar in coffee

--- coffee/src/components/layout.jsx
+++ coffee/src/components/layout.jsx
@@ -1,19 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from "react"
+import PropTypes from "prop-types"
 
-import React from 'react'
-import PropTypes from 'prop-types'
+import "./bootstrap.min.css"
+import "./layout.css"
+import Navbar from "./Globals/Navbar"
 
-import './layout.css'
-
-const Layout = ({ children }) => <> {children} </>
+const Layout = ({ children }) => (
+  <>
+    <Navbar></Navbar> {children}{" "}
+  </>
+)
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- coffee/src/pages/index.jsx
+++ coffee/src/pages/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react"
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+import { FaGulp } from "react-icons/fa"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title='Home' />
-    <h1> Hi people </h1> <p> Welcome to your new Gatsby site. </p>{' '}
+    <SEO title="Home" />
+    <h1> Hi people </h1> <p> Welcome to your new Gatsby site. </p>
+    <FaGulp></FaGulp>
   </Layout>
 )
 
