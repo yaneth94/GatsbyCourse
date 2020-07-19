573ce76d4623b48ef1a2a66f2584376256f03195
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-19 11:07:38 -0600

    added backgroundSection

--- coffee/src/pages/index.jsx
+++ coffee/src/pages/index.jsx
@@ -1,16 +1,29 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-import { FaGulp } from "react-icons/fa"
-
-const IndexPage = () => (
+import BackgroundSection from "../components/Globals/BackgroundSection"
+const IndexPage = ({ data }) => (
   <Layout>
-    <SEO title="Home" />
-    <h1> Hi people </h1> <p> Welcome to your new Gatsby site. </p>
-    <FaGulp></FaGulp>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
