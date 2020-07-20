21436e82c9d0e07c5208fb448aef256e6c27b735
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-20 16:04:53 -0600

    added index page

---
+++ tango-gatsby/src/components/index/About.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+
+import { AboutWrapper } from './styles/AboutStyles'
+
+const About = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wordpressPage(wordpress_id: { eq: 47 }) {
+        content
+        featured_media {
+          localFile {
+            childImageSharp {
+              fluid(quality: 100, maxWidth: 2000) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <AboutWrapper>
+      <div className='container-fluid'>
+        <div className='row'>
+          <div
+            className='col-md-6 aboutImage'
+            style={{
+              backgroundImage: `url(${data.wordpressPage.featured_media.localFile.childImageSharp.fluid.src})`
+            }}
+          />
+          <div className='col-md-6 aboutText'>
+            <div
+              dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }}
+            />
+          </div>
+        </div>
+      </div>
+    </AboutWrapper>
+  )
+}
+
+export default About
