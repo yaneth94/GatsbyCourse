9fc63049b5e3017e3345c659069d67359fa50ab0
Zoila Villatoro <zoilavillatoro6694@gmail.com>
2020-07-21 10:33:01 -0600

    added page template

--- tango-gatsby/gatsby-node.js
+++ tango-gatsby/gatsby-node.js
@@ -1,7 +1,45 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path")
+const slash = require("slash")
 
-// You can delete this file if you're not using it
+exports.createPages = async({ graphql, actions }) => {
+    const { createPage } = actions
+
+    const pageTemplate = path.resolve("./src/templates/page.jsx")
+
+    const result = await graphql(`
+    {
+      allWordpressPage {
+        edges {
+          node {
+            id
+            status
+            link
+            wordpress_id
+            wordpress_parent
+          }
+        }
+      }
+    }
+  `)
+
+    // Check for errors
+    if (result.errors) {
+        throw new Error(result.errors)
+    }
+
+    const { allWordpressPage } = result.data
+
+    allWordpressPage.edges.forEach(edge => {
+        if (edge.node.status === "publish") {
+            createPage({
+                path: edge.node.link,
+                component: slash(pageTemplate),
+                context: {
+                    id: edge.node.id,
+                    parent: edge.node.wordpress_parent,
+                    wpId: edge.node.wordpress_id,
+                },
+            })
+        }
+    })
+}
